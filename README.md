b511ac47368b93540822262de8d2a1a019b45473
digital230 <digit230@gmail.com>
2018-09-16 01:52:28 +0500

    files

---
+++ README.md
@@ -0,0 +1,25 @@
+# react-component
+create react component from command line
+
+ install : npm i -g react-file;
+
+ use: react-file name -e jsx -p -s css -d -r ;
+
+ use 2: react-file name -e -S -s -d;
+
+
+# params:
+
+# -alias --fullname <required> [optional] 'discription' 'default'
+
+'-e, --ext <type>', 'extention of file', 'js';
+
+'-p, --pureC [type]', 'extend with PureComponent', false;
+
+'-s, --style [type]', 'include (or)css file';
+
+'-d, --dir', 'create new directory', false;
+
+'-r, --withRedux', 'file with redux', false;
+
+'-S, --stateless', 'stateless component', false;
---
+++ index.js
@@ -0,0 +1,72 @@
+#! /usr/bin/env node
+
+var program = require('commander');
+var fs = require('fs');
+
+
+function run(name, options) {
+
+  var comp = options.pureC ? 'PureComponent' : 'Component';
+  var ext = options.ext;
+  var stylesheet = options.style & options.style == true ? 'css' : options.style;
+  var dir = options.dir;
+  var withRedux = options.withRedux;
+  var stateless = options.stateless;
+
+  if (stylesheet && !['css', 'scss', 'less'].includes(stylesheet)) {
+    console.log('stylesheet file name not valid exit code:2');
+    return;
+  }
+
+  var className = name.charAt(0).toUpperCase() + name.substr(1);
+  var classNameFull = `${className}.${ext}`;
+  var dirName = className.toLowerCase();
+  var filePath, ssPath, fileContent = '';
+
+  if (dir == true) {
+    if (!fs.existsSync(dirName)) fs.mkdirSync(dirName);
+    filePath = `${dirName}/${classNameFull}`;
+    ssPath  = `${dirName}/${dirName}.${stylesheet}`
+  } else {
+    filePath = `${classNameFull}`;
+    ssPath  = `${dirName}.${stylesheet}`
+  }
+
+  if (filePath && fs.existsSync(filePath)) {
+    console.log('File already present exit code:2');
+    return;
+  }
+
+  if (!stateless) {
+    fileContent = require('./fileContent/content.js')(className, comp, withRedux, {ssPath: `${dirName}.${stylesheet}`, stylesheet})
+  } else {
+    fileContent = require('./fileContent/stateless.js')(className,{ssPath: `${dirName}.${stylesheet}`, stylesheet})
+  }
+
+  fs.writeFile(filePath, fileContent , (err) => {
+    if (err) throw err;
+    else {
+      if (!stylesheet || fs.existsSync(ssPath)) return;
+
+      try{
+        fs.writeFileSync(ssPath, '');
+      }catch (e){
+        console.log("Cannot write file ", e);
+      }
+    }
+  });
+
+}
+
+program
+  .version('1.0.0')
+  .option('-e, --ext <type>', 'extention of file', 'js')
+  .option('-p, --pureC [type]', 'extend with PureComponent', false)
+  .option('-s, --style [type]', 'include (or)css file')
+  .option('-d, --dir [type]', 'create new directory', false)
+  .option('-r, --withRedux [type]', 'file with redux', false)
+  .option('-S, --stateless [type]', 'stateless component', false)
+  .arguments('<name>')
+  .action(run)
+  .parse(process.argv);
+
